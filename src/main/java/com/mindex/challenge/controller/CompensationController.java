9d44ea7869c34f9604c1d813063fa21da8748da2
Gage Askegard <gageaskegard@gages-mbp.lan>
2023-09-16 23:08:15 -0500

    First pass at new Compensation domain

---
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -0,0 +1,31 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.exceptions.EmployeeNotFoundException;
+import com.mindex.challenge.service.CompensationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CompensationController {
+    private static final Logger LOG = LoggerFactory.getLogger(CompensationController.class);
+
+    @Autowired
+    private CompensationService compensationService;
+
+    @PostMapping("/compensation")
+    public Compensation create(@RequestBody Compensation compensation) {
+        LOG.debug("Received compensation create request for [{}]", compensation);
+
+        return compensationService.create(compensation);
+    }
+
+    @GetMapping("/employee/{employeeId}/compensation")
+    public Compensation findByEmployeeId(@PathVariable String employeeId) throws EmployeeNotFoundException {
+        LOG.debug("Received find compensation by employee id request for employeeId [{}]", employeeId);
+
+        return compensationService.findByEmployeeId(employeeId);
+    }
+}
